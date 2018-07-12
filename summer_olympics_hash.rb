a0ef3d1e2090736950814d08b65fef3dc19ec9ee
wue19 <wue19@bcdschool.org>
2018-07-12 02:59:55 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -10,7 +10,7 @@ end
 def add_a_key_value_pair
   # Implement this method so that it adds a key value pair to the hash created in create_olympics_hash
   new_hash = create_olympics_hash
-  new_hash[:Atlanta ]
+  new_hash[:Atlanta]
 end
 
 def iterate_through_hash
