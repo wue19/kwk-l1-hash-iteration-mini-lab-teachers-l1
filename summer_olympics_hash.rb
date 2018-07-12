328910596cfb47c477d63a3d58a7032b08daab51
wue19 <wue19@bcdschool.org>
2018-07-12 03:03:26 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -25,5 +25,5 @@ end
 def iterate_through_keys
   # Implement this method so that it converts the keys of the hash created in add_a_key_value_pair
   # into uppercased Strings, creates a new array with these values, and then puts each upcase key in the CLI
-  upcase_cities = add_a_key_value_pair.map
+  upcase_cities = add_a_key_value_pair.map {|key,value
 end
